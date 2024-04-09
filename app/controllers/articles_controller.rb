c37c6c14e5abd6958db3c66245561b2538b4b214
MannaPriyanka <mannapriyanka04@gmail.com>
2024-04-09 19:48:22 +0530

    Use resources to set routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
     if @article.save
       redirect_to articles_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
     if @article.update(articles_params)
       redirect_to articles_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/article/new", to: "articles#new"
-  post "/articles", to: "articles#create"
-  get "/articles/:id/edit", to: "articles#edit", as: "edit_article"
-  patch "/articles/:id", to: "articles#update"
-  get "/articles/:id", to: "articles#show" ,as: :article
-  delete "/articles/:id", to: "articles#destroy"
+
+  # get "/articles", to: "articles#index"
+  # get "/article/new", to: "articles#new"
+  # post "/articles", to: "articles#create"
+  # get "/articles/:id/edit", to: "articles#edit", as: "edit_article"
+  # patch "/articles/:id", to: "articles#update"
+  # get "/articles/:id", to: "articles#show" ,as: :article
+  # delete "/articles/:id", to: "articles#destroy"
+
+  resources :articles
+
 end
