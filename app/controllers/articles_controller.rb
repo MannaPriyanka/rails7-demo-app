975f4cdcfaebec43d84cb8e09a7ba397eb12446a
MannaPriyanka <mannapriyanka04@gmail.com>
2024-04-09 19:20:32 +0530

    CRUD operations on articles with basic routes and views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,48 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: %i[show edit update destroy]
   def index
     @articles = Article.all
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(articles_params)
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @article.destroy!
+    redirect_to articles_path
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   get "/articles", to: "articles#index"
+  get "/article/new", to: "articles#new"
+  post "/articles", to: "articles#create"
+  get "/articles/:id/edit", to: "articles#edit", as: "edit_article"
+  patch "/articles/:id", to: "articles#update"
+  get "/articles/:id", to: "articles#show" ,as: :article
+  delete "/articles/:id", to: "articles#destroy"
 end
